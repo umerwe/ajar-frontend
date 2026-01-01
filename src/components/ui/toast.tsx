4b18845fdceb405c17a6baca29d47f753831cd27
umerwe <uemyy1@gmail.com>
2026-01-01 15:39:16 +0500

    updated

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -12,9 +12,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      // CHANGED: Added 'hidden md:flex'. 
-      // This hides the toast container on mobile/sm screens and shows it only on md+ screens.
-      "fixed top-0 z-[100] hidden md:flex max-h-screen w-full flex-col-reverse gap-2 p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse gap-2 p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]",
       className,
     )}
     {...props}
