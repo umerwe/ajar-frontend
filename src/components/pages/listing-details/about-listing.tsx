76461cf6160b8643273d23d1fd64f26499eea393
umerwe <uemyy1@gmail.com>
2026-01-01 17:49:01 +0500

    updated

--- src/components/pages/listing-details/about-listing.tsx
+++ src/components/pages/listing-details/about-listing.tsx
@@ -4,9 +4,9 @@ const AboutListing = ({ property }: { property: Listing }) => {
     return (
         <div>
             <h2 className="text-lg md:text-xl font-semibold text-gray-800 mt-3">About</h2>
-            <p className="text-gray-600 text-sm md:text-base max-w-full lg:max-w-130">
+            <p className="text-gray-600 text-sm md:text-base max-w-full lg:max-w-130 first-letter:uppercase">
                 <span>
-                    {property.subTitle}
+                    {property.description || 'No description available.'}
                 </span>
             </p>
         </div>
