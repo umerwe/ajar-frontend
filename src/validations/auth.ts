2bdedc465b59e4419e586d2bccada16df84be22a
umerwe <uemyy1@gmail.com>
2026-01-14 14:53:24 +0500

    updated registerSchema validations

--- src/validations/auth.ts
+++ src/validations/auth.ts
@@ -7,7 +7,7 @@ export const LoginSchema = z.object({
 });
 
 export const RegisterSchema = z.object({
-  name: z.string().min(1, "Name is required"),
+  name: z.string().min(3, "Name must be at least 3 characters "),
   email: z.string().email("Invalid email address"),
   password: z.string().min(8, "Password must be at least 8 characters"),
   confirmPassword: z.string().min(8, "Confirm Password is required"),
