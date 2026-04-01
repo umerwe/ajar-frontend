63c53091cef1c1e43b3dac9746e94eaa00ad3a84
umerwe <uemyy1@gmail.com>
2026-04-01 12:37:42 +0500

    removed isGuestFavourite Check

--- src/components/cards/card-header.tsx
+++ src/components/cards/card-header.tsx
@@ -45,13 +45,6 @@ const CardTop = ({ property, showRemoveButton = false, type }: CardTopProps) => 
       className="p-0 relative"
     >
       <div className="absolute top-2 sm:top-3 left-4 right-2 sm:right-4 flex justify-between items-center z-10">
-        <div>
-          {property.isGuestFavorite && (
-            <div className="bg-white/90 backdrop-blur-sm px-3 rounded-full hidden sm:block pb-1">
-              <span className="text-xs font-semibold">Guest favorite</span>
-            </div>
-          )}
-        </div>
         <div
           onClick={(e) => {
             e.preventDefault();
