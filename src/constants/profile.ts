dd6a088241f25beb4f3f3c828194f2113e3e088d
umerwe <uemyy1@gmail.com>
2026-08-12 18:01:48 +0500

    added key in profileItems

--- src/constants/profile.ts
+++ src/constants/profile.ts
@@ -12,49 +12,59 @@ export const profileMenuItems = [
 export const profileItems = [
     {
         icon: Pencil,
+        key: "editProfile",
         label: "Edit Profile"
     },
     {
         icon: Wallet,
+        key: "transactions",
         label: "Transactions",
         href: "/transactions"
     },
     {
         icon: CalendarCheck,
+        key: "bookings",
         label: "Bookings",
         href: "/booking/all",
     },
     {
         icon: Landmark,
+        key: "connectBankAccount",
         label: "Connect Bank Account",
         href: "/connect-bank-account",
     },
     {
         icon: Heart,
+        key: "favorites",
         label: "Favorites",
         href: "/favourites",
     },
     {
         icon: HelpCircle,
+        key: "faqs",
         label: "FAQs",
         href: "/faqs",
     },
     {
         icon: Pencil,
+        key: "addDocuments",
         label: "Add Documents"
     },
     {
         icon: Lock,
+        key: "changePassword",
         label: "Change Password",
         href: "/change-password",
     },
     {
         icon: ShieldCheck,
+        key: "twoFactorAuthentication",
         label: "Two-Factor Authentication",
         href: "/two-factor",
     },
     {
         icon: Pencil,
+        key: "refundRequest",
         label: "Refund Request",
         href: "/refund",
     }
