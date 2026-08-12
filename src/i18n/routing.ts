05bdc44b722c011bd1591245f59d40837d5f9999
umerwe <uemyy1@gmail.com>
2026-08-12 18:01:22 +0500

    added en and ar locales

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,8 +1,15 @@
 import {defineRouting} from 'next-intl/routing';
+
+export const locales = ['en', 'ar'] as const;
+
+export const localeLabels: Record<(typeof locales)[number], string> = {
+  en: 'English',
+  ar: 'Arabic',
+};
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales,
  
   // Used when no locale matches
   defaultLocale: 'en'
